478913801ea11e36ad8663d69e25f89ccd024915
Tamer Fouad <tamer.fbhb.wd@gmail.com>
2024-02-19 00:27:32 +0200

    ed

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         <div className="container">
           <NavBar />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </body>
     </html>
