6308b2168cb40965a346b23e8006146b92151423
Tamer Fouad <tamer.fbhb.wd@gmail.com>
2024-02-18 15:41:36 +0200

    ed

---
+++ src/app/(auth-page)/about/page.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from './About.module.scss'
+
+export default function page() {
+    return (
+        <div className={styles.about}>
+            <div className={styles.inform}>
+                <h1>Together let's discover the world</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse obcaecati et quibusdam. Eum ea consectetur, aperiam explicabo a omnis, odio excepturi sint iusto quia vitae. Voluptates, natus. Accusantium harum nobis, laudantium nam praesentium voluptatem pariatur porro suscipit iure, dolor amet?</p>
+       
+            </div>
+            <div className={styles.ContainerAvatar}>
+                <div className={styles.avatar}>
+                    <Image src="/Untitled.png" alt="avatar" loading="lazy" fill className={styles.img} />
+                </div>
+            </div>
+        </div>
+    )
+};
